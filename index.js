176d9440d2c53fa7f389bcbf2ccf0b4d8086a451
Chima Chukwuemeka <chukwuemekachm@gmail.com>
2018-08-24 11:55:36 +0100

    chore(express): Finishes setup of express server (#4)
    - Install and configure express to serve the application
    - Write tests for the server
    
    [Finishes #159730870]

---
+++ index.js
@@ -0,0 +1,15 @@
+import expess from 'express';
+import path from 'path';
+
+const app = expess();
+const PORT = process.env.PORT || 3000;
+
+app.use(expess.static('client/app'));
+
+app.all('*', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'client', 'app', 'index.html'));
+});
+
+app.listen(PORT);
+
+export default app;
